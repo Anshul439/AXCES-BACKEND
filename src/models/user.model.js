0bf72a5033f971839b76e92f9dd7e1d50c312280
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-07-27 00:15:54 +0530

    Fix some bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,11 +9,9 @@ const UserSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
     },
     email: {
       type: String,
-      required: true,
       unique: true,
     },
 
